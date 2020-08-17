7481e3bc00ca19ec3d7f2b84122293d5feb2ca77
William Rocha <william_rocha123@hotmail.com>
2020-08-16 21:37:18 -0300

    feat: fix port env

--- common/environment.ts
+++ common/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  server: { port: process.env.SERVER_PORT || 3000 },
+  server: { port: process.env.PORT || 3000 },
   db: {url: process.env.DB_URL || 'mongodb://localhost/meat-api'},
   security: {
     saltRounds: process.env.SALT_ROUNDS || 10,
