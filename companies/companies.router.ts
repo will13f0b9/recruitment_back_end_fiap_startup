878ec545e9e7cae0ca12487fb2f86a6c30068fca
William Rocha <william_rocha123@hotmail.com>
2020-09-26 23:57:52 -0300

    feat: company router without authorization

--- companies/companies.router.ts
+++ companies/companies.router.ts
@@ -1,38 +1,38 @@
-import {ModelRouter} from '../common/model-router'
+import { ModelRouter } from '../common/model-router'
 import * as restify from 'restify'
-import {NotFoundError} from 'restify-errors'
-import {Company} from './companies.model'
-import {authorize} from '../security/authz.handler'
+import { NotFoundError } from 'restify-errors'
+import { Company } from './companies.model'
+import { authorize } from '../security/authz.handler'
 
 class CompaniesRouter extends ModelRouter<Company> {
-  constructor(){
+  constructor() {
     super(Company)
   }
 
 
-  findByCnpj = (req, resp, next)=>{
-    if(req.query.cnpj){
+  findByCnpj = (req, resp, next) => {
+    if (req.query.cnpj) {
       console.log(`Buscando por CNPJ=${req.query.cnpj}`)
       Company.findByCnpj(req.query.cnpj)
-          .then(company => company ? [company] : [])
-          .then(this.renderAll(resp, next, {
-                pageSize: this.pageSize,
-                url: req.url
-              }))
-          .catch(next)
-    }else{
+        .then(company => company ? [company] : [])
+        .then(this.renderAll(resp, next, {
+          pageSize: this.pageSize,
+          url: req.url
+        }))
+        .catch(next)
+    } else {
       next()
     }
   }
 
-  applyRoutes(application: restify.Server){
+  applyRoutes(application: restify.Server) {
     application.get(`${this.basePath}`, [this.findByCnpj, this.findAll])
 
     application.get(`${this.basePath}/:id`, [this.validateId, this.findById])
     application.post(`${this.basePath}`, [this.save])
-    application.put(`${this.basePath}/:id`, [authorize('admin'),this.validateId,this.replace])
-    application.patch(`${this.basePath}/:id`, [authorize('admin'),this.validateId,this.update])
-    application.del(`${this.basePath}/:id`, [authorize('admin'),this.validateId,this.delete])
+    application.put(`${this.basePath}/:id`, [this.validateId, this.replace])
+    application.patch(`${this.basePath}/:id`, [this.validateId, this.update])
+    application.del(`${this.basePath}/:id`, [this.validateId, this.delete])
 
   }
 
--- dist/companies/companies.router.js
+++ dist/companies/companies.router.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const model_router_1 = require("../common/model-router");
 const companies_model_1 = require("./companies.model");
-const authz_handler_1 = require("../security/authz.handler");
 class CompaniesRouter extends model_router_1.ModelRouter {
     constructor() {
         super(companies_model_1.Company);
@@ -26,9 +25,9 @@ class CompaniesRouter extends model_router_1.ModelRouter {
         application.get(`${this.basePath}`, [this.findByCnpj, this.findAll]);
         application.get(`${this.basePath}/:id`, [this.validateId, this.findById]);
         application.post(`${this.basePath}`, [this.save]);
-        application.put(`${this.basePath}/:id`, [authz_handler_1.authorize('admin'), this.validateId, this.replace]);
-        application.patch(`${this.basePath}/:id`, [authz_handler_1.authorize('admin'), this.validateId, this.update]);
-        application.del(`${this.basePath}/:id`, [authz_handler_1.authorize('admin'), this.validateId, this.delete]);
+        application.put(`${this.basePath}/:id`, [this.validateId, this.replace]);
+        application.patch(`${this.basePath}/:id`, [this.validateId, this.update]);
+        application.del(`${this.basePath}/:id`, [this.validateId, this.delete]);
     }
 }
 exports.companiesRouter = new CompaniesRouter();
